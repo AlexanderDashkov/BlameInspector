3c0c8d47c664bb1428af8a43830fc4ef199ed9e1
JaneSmithSenior <Dashkov.Alexander.OM@gmail.com>
2016-03-27 19:02:24 +0300

    Source enhancment partly

--- BlameInspector/src/main/java/BlameInspector/PropertyService.java
+++ BlameInspector/src/main/java/BlameInspector/PropertyService.java
@@ -3,8 +3,10 @@ package BlameInspector;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
+import org.xml.sax.ErrorHandler;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
+import org.xml.sax.SAXParseException;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -106,5 +108,19 @@ public class PropertyService {
 
     public String getVersionControl() {
         return versionControl;
+    }
+
+    public class SimpleErrorHandler implements ErrorHandler {
+        public void warning(SAXParseException e) throws SAXException {
+            System.out.println(e.getMessage());
+        }
+
+        public void error(SAXParseException e) throws SAXException {
+            System.out.println(e.getMessage());
+        }
+
+        public void fatalError(SAXParseException e) throws SAXException {
+            System.out.println(e.getMessage());
+        }
     }
 }
